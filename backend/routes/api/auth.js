d05ad9891f2e1f1a03365266344f2ebeb5ec747f
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2021-06-11 17:11:11 +0530

    login user route

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -22,4 +22,65 @@ router.get('/',
   }
 });
 
+// POST api/auth
+// Authenticate user and get token route
+// Public
+// login
+router.post(
+  "/",
+  [
+    check("email", "please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // destructure
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        // if user doesn't exists
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // check if password matches
+      const isMatched = await bcrypt.compare(password, user.password);
+
+      if (!isMatched) {  
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
 module.exports = router;
