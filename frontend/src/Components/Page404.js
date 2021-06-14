0eb966d8306d92f6ce6a9c4a7eb2f91f6f2d701f
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2021-06-14 14:49:51 +0530

    Page 404

---
+++ frontend/src/Components/Page404.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Background from '../images/2634442.jpg';
+
+function Page404() {
+  return <div style={styles}></div>;
+}
+
+const styles = {
+  width: '100%',
+  height: '100vh',
+  backgroundImage: `url(${Background})`,
+  backgroundSize: 'contain',
+  backgroundPositon: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundColor: '#E9FCFF',
+};
+
+export default Page404;
